be2fe7c4525918fc3e1a6565492cb9a11838847d
Joe <jvd5839@g.rit.edu>
2017-08-29 21:09:23 -0400

    insertion, quick

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -11,11 +11,22 @@ def bubble_sort(arr):
 
 
 def quick_sort(arr):
-    pass
+    if not arr:
+        return arr
+    pivot = arr[0]
+    below = [i for i in arr[1:] if i < pivot]
+    above = [i for i in arr[1:] if i >= pivot]
+    return quick_sort(below) + [pivot] + quick_sort(above)
 
 
 def insertion_sort(arr):
-    pass
+    for i in range(1, len(arr)):
+        k = arr[i]
+        j = i - 1
+        while j >= 0  and k < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = k
 
 
 def merge_sort(arr):
@@ -27,7 +38,18 @@ def rand_sort(arr):
 
 if __name__ == '__main__':
     strings = ["hey", "zed", "inhumane", "a", "c", "b", "z", "p"]
-    ints = [int(1000*random.random()) for i in range(100)]
-    t = timeit.Timer(lambda: bubble_sort(ints))
-    print(format(t.timeit(10), '.20f') + " seconds")
+    ints = [int(1000*random.random()) for i in range(50)]
+
+    qsort = timeit.Timer(lambda: quick_sort(ints))
+    print("Quick sort: " + format(qsort.timeit(10), '.20f') + " seconds")
     print(ints)
+
+    isort = timeit.Timer(lambda: insertion_sort((ints)))
+    print("Insertion sort: " + format(isort.timeit(10), '.20f') + " seconds")
+    print(ints)
+
+    bsort = timeit.Timer(lambda: bubble_sort(ints))
+    print("Bubble sort:" + format(bsort.timeit(10), '.20f') + " seconds")
+    print(ints)
+
+
